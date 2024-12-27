0b1908088bd2e483169c5129544ac300f13dcadf
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-12-28 00:32:48 +0100

    Added enums

--- src/enums/timemode.ts
+++ src/enums/timemode.ts
@@ -1,5 +1,28 @@
+export enum TimeMode {
+	Default = 0,
+	Frames120 = 1,
+	Frames100 = 2,
+	Frames60 = 3,
+	Frames50 = 4,
+	Frames48 = 5,
+	Frames30 = 6,
+	Frames30Drop = 7,
+	NtscDropFrame = 8,
+	NtscFullFrame = 9,
+	Pal = 10,
+	Frames24 = 11,
+	Frames1000 = 12,
+	FilmFullFrame = 13,
+	Custom = 14,
+	Frames96 = 15,
+	Frames72 = 16,
+	Frames59_94 = 17,
+	Frames119_88 = 18,
+}
+
+// TODO: remove those
 export const FBX_TIME_MODE_DEFAULT = 0;
-export const FBX_TIME_MODE_FRAMES_120 =1;
+export const FBX_TIME_MODE_FRAMES_120 = 1;
 export const FBX_TIME_MODE_FRAMES_100 = 2;
 export const FBX_TIME_MODE_FRAMES_60 = 3;
 export const FBX_TIME_MODE_FRAMES_50 = 4;
@@ -13,7 +36,7 @@ export const FBX_TIME_MODE_FRAMES_24 = 11;
 export const FBX_TIME_MODE_FRAMES_1000 = 12;
 export const FBX_TIME_MODE_FILM_FULL_FRAME = 13;
 export const FBX_TIME_MODE_CUSTOM = 14;
-export const FBX_TIME_MODE_FRAMES_96 =15;
+export const FBX_TIME_MODE_FRAMES_96 = 15;
 export const FBX_TIME_MODE_FRAMES_72 = 16;
 export const FBX_TIME_MODE_FRAMES_59_94 = 17;
 export const FBX_TIME_MODE_FRAMES_119_88 = 18;
--- src/enums/timeprotocol.ts
+++ src/enums/timeprotocol.ts
@@ -1,3 +1,10 @@
+export enum TimeProtocol {
+	Smpte = 0,
+	FrameCount = 1,
+	Default = 2,
+}
+
+// TODO: remove those
 export const FBX_TIME_PROTOCOL_SMPTE = 0;
-export const FBX_TIME_PROTOCOL_FRAME_COUNT = 0;
-export const FBX_TIME_PROTOCOL_DEFAULT = 0;
+export const FBX_TIME_PROTOCOL_FRAME_COUNT = 1;
+export const FBX_TIME_PROTOCOL_DEFAULT = 2;
