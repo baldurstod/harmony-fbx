a9b3f85a10ea5eab852fb54976ac5a7a927d09af
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-12-27 21:12:02 +0100

    added typescript declarations

--- src/utils/createfbxproperty.ts
+++ src/utils/createfbxproperty.ts
@@ -1,31 +1,30 @@
-import { FBX_DATA_TYPE_DOUBLE, FBX_DATA_TYPE_INT_32, FBX_DATA_TYPE_INT_64, FBX_DATA_TYPE_RAW, FBX_DATA_TYPE_STRING, FBX_DATA_TYPE_INT_16, FBX_DATA_TYPE_ARRAY_INT_8, FBX_DATA_TYPE_ARRAY_DOUBLE, FBX_DATA_TYPE_ARRAY_FLOAT } from '../constants.js';
+import { FBX_DATA_TYPE_DOUBLE, FBX_DATA_TYPE_INT_32, FBX_DATA_TYPE_INT_64, FBX_DATA_TYPE_RAW, FBX_DATA_TYPE_STRING, FBX_DATA_TYPE_INT_16, FBX_DATA_TYPE_ARRAY_INT_8, FBX_DATA_TYPE_ARRAY_DOUBLE, FBX_DATA_TYPE_ARRAY_FLOAT } from '../constants';
+import { FBXRecordProperty } from '../model/fbxrecordproperty';
 
-import {FBXRecordProperty} from '../model/fbxrecordproperty.js';
-
-export function createInt16Property(value) {
+export function createInt16Property(value: number) {
 	return new FBXRecordProperty(null, FBX_DATA_TYPE_INT_16, value);
 }
-export function createInt32Property(value) {
+export function createInt32Property(value: number) {
 	return new FBXRecordProperty(null, FBX_DATA_TYPE_INT_32, value);
 }
-export function createInt64Property(value) {
-	return new FBXRecordProperty(null, FBX_DATA_TYPE_INT_64, BigInt(value));
+export function createInt64Property(value: BigInt) {
+	return new FBXRecordProperty(null, FBX_DATA_TYPE_INT_64, value);
 }
-export function createDoubleProperty(value) {
+export function createDoubleProperty(value: number) {
 	return new FBXRecordProperty(null, FBX_DATA_TYPE_DOUBLE, value);
 }
-export function createRawProperty(value) {
+export function createRawProperty(value: any/*TODO: better type*/) {
 	return new FBXRecordProperty(null, FBX_DATA_TYPE_RAW, value);
 }
-export function createStringProperty(value) {
+export function createStringProperty(value: string) {
 	return new FBXRecordProperty(null, FBX_DATA_TYPE_STRING, value);
 }
 
 
 
-export function createDoubleArrayProperty(value) {
+export function createDoubleArrayProperty(value:Array<number>) {
 	return new FBXRecordProperty(null, FBX_DATA_TYPE_ARRAY_DOUBLE, value);
 }
-export function createFloatArrayProperty(value) {
+export function createFloatArrayProperty(value:Array<number>) {
 	return new FBXRecordProperty(null, FBX_DATA_TYPE_ARRAY_FLOAT, value);
 }
