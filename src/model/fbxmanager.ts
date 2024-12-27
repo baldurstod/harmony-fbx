6f940833aeb734c81be1adc43d975f6b3f62243c
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-12-27 18:33:29 +0100

    FBXManager: added typescript declarations

--- src/model/fbxmanager.ts
+++ src/model/fbxmanager.ts
@@ -1,31 +1,31 @@
-export class FBXManager {
-	#objects = new Set();
-	#documents = new Set();
-	static #registry = new Map();
+import { FBXDocument } from './fbxdocument';
+import { FBXObject } from './fbxobject';
 
-	constructor() {
-		this.isFBXManager = true;
-	}
+export class FBXManager {
+	#objects = new Set<FBXObject>();
+	#documents = new Set<FBXDocument>();
+	isFBXManager = true;
+	static #registry = new Map<string, typeof FBXObject>();
 
 	destroy() {
 		this.#objects.clear();
 		this.#documents.clear();
 	}
 
-	static registerClass(className, classConstructor) {
+	static registerClass(className: string, classConstructor: typeof FBXObject) {
 		FBXManager.#registry.set(className, classConstructor);
 	}
 
-	createObject(className, objectName, ...args) {
+	createObject(className: string, objectName: string, ...args: Array<any>) {
 		const classConstructor = FBXManager.#registry.get(className);
 		if (!classConstructor) {
 			throw 'Unknown constructor in FBXManager.createObject(): ' + className;
 		}
 
-		const createdObject = new classConstructor(this, objectName, ...args);
+		const createdObject = new classConstructor(this, objectName, args);
 		if (createdObject) {
-			if (createdObject.isFBXDocument) {
-				this.#documents.add(createdObject);
+			if ((createdObject as FBXDocument).isFBXDocument) {
+				this.#documents.add(createdObject as FBXDocument);
 			} else {
 				this.#objects.add(createdObject);
 			}
