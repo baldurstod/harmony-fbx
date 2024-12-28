b8db5cb4f5ca06156ef2246cbe72c43a6c75ce37
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-12-28 14:52:18 +0100

    Added enum

--- src/enums/skeletontype.ts
+++ src/enums/skeletontype.ts
@@ -1,3 +1,11 @@
+export enum SkeletonType {
+	Root = 0,
+	Limb = 1,
+	LimbNode = 2,
+	Effector = 3,
+}
+
+// TODO: remove these
 export const FBX_SKELETON_TYPE_ROOT = 0;
 export const FBX_SKELETON_TYPE_LIMB = 1;
 export const FBX_SKELETON_TYPE_LIMB_NODE = 2;
